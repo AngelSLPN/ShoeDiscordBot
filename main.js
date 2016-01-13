f035d05f07e746efd9de7ab63a02119fc21cf57c
EC2 Default User <ec2-user@ip-172-31-45-217.us-west-2.compute.internal>
2016-01-13 01:44:09 +0000

    added some test commands and improved support for adding new commands

--- main.js
+++ main.js
@@ -6,6 +6,63 @@ var Discord = require('discord.js'),
 var mybot = new Discord.Client(),
     splatnet = new SplatnetScraper();
 
+var commands = {
+  help: {
+    cooldown: 0,
+    help: 'call this command to get help',
+    script: function(message, args) {
+      if (args[0]) {
+        mybot.sendMessage(message.channel, commands[args[0]].help);
+      } else {
+        //list commands
+        mybot.sendMessage(message.channel, Object.keys(commands).toString());
+      }
+    },
+  },
+  servers: {
+    cooldown: 600,
+    help: 'a list of servers this bot is in',
+    script: function(message, args) {
+      mybot.sendMessage(message.channel, mybot.servers);
+    },
+  },
+  channels: {
+    cooldown: 600,
+    help: 'a list of channels this bot is in',
+    script: function(message, args) {
+      mybot.sendMessage(message.channel, mybot.channels);
+    },
+  },
+  schedule: {
+    cooldown: 600,
+    help: 'upcoming map rotations in splatoon',
+    script: function(message, args) {
+      async.series([
+        splatnet.login,
+        splatnet.getSchedule.bind(splatnet),
+      ], function(err, results) {
+        if (results) {
+          mybot.sendMessage(message.channel, results[1]);
+        }
+      });
+    },
+  },
+  ranked: {
+    cooldown: 600,
+    help: 'test command to get ranks',
+    script: function(message, args) {
+      async.series([
+        splatnet.login,
+        splatnet.getRanked.bind(splatnet),
+      ], function(err, results) {
+        /*if (results) {
+          mybot.sendMessage(message.channel, results[1]);
+        }*/
+      });
+    },
+  },
+}
+
 //when the bot is ready
 mybot.on("ready", function () {
 	console.log("Ready to begin! Serving in " + mybot.channels.length + " channels");
@@ -21,20 +78,22 @@ mybot.on("disconnected", function () {
 });
 
 mybot.on('message', function(message) {
-  if (message.content === 'ping') {
-    mybot.reply(message, 'pong');
-  }
-  if (message.content === '%schedule') {
-
-    async.series([
-      splatnet.login,
-      splatnet.getSchedule.bind(splatnet),
-    ], function(err, results) {
-      if (results) {
-        mybot.sendMessage(message.channel, results[1]);
-      }
-    });
+  if (message.content.startsWith('%')) {
+    var parsed = parseCommand(message.content);
+    if (commands.hasOwnProperty(parsed.command)) {
+      commands[parsed.command].script(message, parsed.arguments);
+    } else {
+      mybot.sendMessage(message.channel, command + ' command not supported');
+    }
   }
 });
 
-mybot.login('hhhhsu@yahoo.com', 'ibAndroid4389Bot');
+function parseCommand(messageContent) {
+  var args = messageContent.slice(1).split(' ');
+  return {
+    command: args[0],
+    arguments: args.slice(1),
+  }
+}
+
+mybot.login(auth.discord.email, auth.discord.password);
